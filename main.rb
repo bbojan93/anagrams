43873ff8b1ca3890a17552c25f882734ff3d9b6f
Bojan Babic <bojance93@gmail.com>
2017-10-10 23:07:26 +0200

    commit

--- main.rb
+++ main.rb
@@ -3,7 +3,8 @@ require './lib/anagrams.rb'
 puts "insert a file name"
 name = gets.chomp
 anagram = Anagram.new(name)
+
 puts "Anagrams found:"
 puts "#{anagram.count_anagrams}"
 
-#anagram.show_anagram
+anagram.show_anagram
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -4,12 +4,21 @@ describe Anagram do
 
   describe "#count_anagrams" do
 
+    context "given a file test.txt" do
+      it "should return 4 anagrams" do
+        anagram = Anagram.new("test.txt")
+        expect(anagram.count_anagrams).to eql(4)
+      end
+    end
+
     context "given a file anagrams-wordlist.txt" do
       it "should return 22683 anagrams" do
         anagram = Anagram.new("anagrams-wordlist.txt")
         expect(anagram.count_anagrams).to eql(22683)
       end
     end
+
+    context
 
 
   end
