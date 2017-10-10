8cf04b1234ae5b8a04db12fe1a1a7b16f10d7621
Bojan Babic <bojance93@gmail.com>
2017-10-10 02:59:15 +0200

    Modify methods

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,29 +1,36 @@
 class Anagram
-  
+  attr_reader :result
+
   def initialize(file_name)
     @file = File.open(file_name)
   end
 
   def find_anagrams
-    result = Hash.new { |hash, key| hash[key] = [] }
+    @result = Hash.new { |hash, key| hash[key] = [] }
 
     @file.each_line do |word|
       letters_sorted = word.downcase.chars.sort.join
 
-      result[letters_sorted] << word
+      @result[letters_sorted] << word.chomp
     end
 
-    result
+    @result
   end
 
-  def count_anagrams(result)
+  def count_anagrams
     counter = 0
-    result.each do |k, v|
+    @result.each do |k, v|
       if v.length > 1
         counter += 1
       end
     end
     counter
+  end
+
+  def show_anagrams
+    @result.each do |k, v|
+      puts v.join(", ")
+    end
   end
 
 end
--- main.rb
+++ main.rb
@@ -3,5 +3,8 @@ require './lib/anagrams.rb'
 puts "insert a file name"
 name = gets.chomp
 anagram = Anagram.new(name)
-puts "number of possible anagrams is:"
-puts "#{anagram.count_anagrams(anagram.find_anagrams)}"
+anagram.find_anagrams
+puts "Anagrams found:"
+puts "#{anagram.count_anagrams}"
+
+#anagram.show_anagrams
