21b1ce13fe282be1ae6b9d82a597656e9250f685
Bojan Babic <bojance93@gmail.com>
2017-10-10 18:45:44 +0200

    modify

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -5,21 +5,30 @@ class Anagram
     @file = File.open(file_name)
   end
 
-  def find_anagrams
-    result = Hash.new { |hash, key| hash[key] = [] }
-
-    @file.each_line do |word|
-      letters_sorted = word.downcase.chars.sort.join
-
-      result[letters_sorted] << word.chomp
+  def encode_file
+    encoded = []
+    @file.each do |word|
+      encoded << word.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
     end
-
-    result
+    encoded
   end
 
-  def count_anagrams(result)
+  def find_anagrams
+    @result = Hash.new { |hash, key| hash[key] = [] }
+
+    encode_file.each do |word|
+      letters_sorted = word.downcase.chars.sort.join
+
+      @result[letters_sorted] << word.chomp
+    end
+
+    @result
+  end
+
+  def count_anagrams
+    find_anagrams
     counter = 0
-    result.each do |k, v|
+    @result.each do |k, v|
       if v.length > 1
         counter += 1
       end
@@ -27,8 +36,8 @@ class Anagram
     counter
   end
 
-  def show_anagrams(result)
-    result.each do |k, v|
+  def show_anagram
+    @result.each do |k, v|
       puts v.join(", ") if v.length>1
     end
   end
--- main.rb
+++ main.rb
@@ -4,7 +4,6 @@ puts "insert a file name"
 name = gets.chomp
 anagram = Anagram.new(name)
 puts "Anagrams found:"
-result = anagram.find_anagrams
-puts "#{anagram.count_anagrams(result)}"
+puts "#{anagram.count_anagrams}"
 
-anagram.show_anagrams(result)
+anagram.show_anagram
