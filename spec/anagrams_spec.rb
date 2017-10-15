64295cc89548cffa4443d3ce07baebceb508bda4
Bojan Babic <bojance93@gmail.com>
2017-10-15 23:50:27 +0200

    Add test

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -2,6 +2,15 @@ require 'anagrams.rb'
 
 describe Anagram do
 
+  describe "#sort_word" do
+
+    context "given a word listen" do
+      it "should return eilnst" do
+        expect(Anagram.new("test.txt").sort_word('listen')).to eql('eilnst')
+      end
+    end
+  end
+
   describe "#count_anagrams" do
 
     context "given a file test.txt" do
@@ -12,15 +21,12 @@ describe Anagram do
     end
 
     context "given a file anagrams-wordlist.txt" do
-      it "should return 22683 anagrams" do
+      it "should return 30598 anagrams" do
         anagram = Anagram.new("anagrams-wordlist.txt")
-        expect(anagram.count_anagrams).to eql(22683)
+        expect(anagram.count_anagrams).to eql(30598)
       end
     end
-
-    context
-
-
   end
+
 
 end
